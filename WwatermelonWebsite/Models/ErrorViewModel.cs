b46740409f2622f1a3ee662a88be102ad97d9404
araoofi <araoofi@hawk.iit.edu>
2024-11-26 16:27:05 -0600

    new cs project

---
+++ WwatermelonWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WwatermelonWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
