53458f99cf4dad8d2deae6d18a284f7ffa65ca1a
araoofi <araoofi@hawk.iit.edu>
2024-11-26 16:44:56 -0600

    added AboutUs and RequestInfo

--- WwatermelonWebsite/obj/Release/net6.0/WwatermelonWebsite.AssemblyInfo.cs
+++ WwatermelonWebsite/obj/Release/net6.0/WwatermelonWebsite.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WwatermelonWebsite")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698511bd89cec06711ef44f9bbe59920c81b5a90")]
 [assembly: System.Reflection.AssemblyProductAttribute("WwatermelonWebsite")]
